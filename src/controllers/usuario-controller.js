5c7ee7c17ab551455a87c353323dfa96b2a55105
Caroline <carolnte@gmail.com>
2021-01-24 21:40:29 -0300

    Tudo certo para o CRUD

--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -1,15 +1,59 @@
-class usuarioController {
+const Usuario = require('../models/usuario-model')
 
-    static getUsuario(){
-        return( (req,resp)=>
-        resp.send("<h1>Rota ativada com GET e recurso usuários</h1>"))
-    }
 
-    static postUsuario(){
-        return( (req,resp)=>
-        resp.send("<h1>Rota POST de usuário ativada: usuário adicionado ao banco de dados.>"))
-    }
+module.exports = (app, bd) => {
+    app.get('/usuarios', (req, resp) => 
+        {
+            resp.send(bd.usuariosBD)
+        }
+    )
 
+    app.get('/usuarios/:email', (req, resp)=>
+        {
+            for (let usr of bd.usuariosBD)
+            {
+                if(req.params.email == usr.email){resp.send(usr)}
+            }
+
+            resp.send("Usuário não encontrado")
+
+        }
+    )
+
+    app.post('/usuarios', (req, resp) => 
+        {
+            const usr = new Usuario(req.body.nome, req.body.email, req.body.senha)
+            bd.usuariosBD.push(usr)
+            resp.send('<h1>Usuário adicionado ao banco de dados</h1>')
+        }
+    )
+
+    app.delete('/usuarios/:email', (req,resp)=>
+        {
+            for(let i =0; i <bd.usuariosBD.length; i++)
+            {
+                if(req.params.email == bd.usuariosBD[i].email)
+                {bd.usuariosBD.splice(i,1)
+                resp.send("Usuário deletado")}
+            }
+
+            resp.send("Usuário não encontrado")
+        }
+    )
+
+    app.put('/usuarios/:email', (req,resp)=>
+        {
+            for (usr of bd.usuariosBD)
+            {
+                if(req.params.email == usr.email)
+                {usr.nome = req.body.nome
+                 usr.senha = req.body.senha
+                 resp.send("Usuário alterado")}
+            }
+
+            resp.send("Usuário não encontrado")
+        }
+    
+    )
 }
 
-module.exports = usuarioController;
--- src/models/usuario-model.js
+++ src/models/usuario-model.js
@@ -1,2 +1,10 @@
+class usuarioModel{
 
+    constructor(nome, email, senha){
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+}
 
+module.exports = usuarioModel;
