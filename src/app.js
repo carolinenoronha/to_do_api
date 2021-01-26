0a7a569fa79c3056e8cea737ce5a6d55cf666714
Caroline <carolnte@gmail.com>
2021-01-26 16:06:24 -0300

    Importação do SQLite DB adicionada

--- src/app.js
+++ src/app.js
@@ -2,8 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000 || 8080;
-const bd = require('./infra/bd')
+const bd = require('./infra/sqlite-db')/* require('./infra/bd'); */
+/* const db =  */
+const sqlite = require('sqlite3');
+
+
+
 app.use(bodyParser.json());
+
+
 const tarefaController = require('./controllers/tarefa-controller')
 tarefaController(app, bd);
 
