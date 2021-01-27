4209fae903e45bfb9357aaddc0ec8b7c7d887f8c
Caroline <carolnte@gmail.com>
2021-01-27 16:27:52 -0300

    Modificãções feitas para utilização do DAO

--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -1,15 +1,20 @@
-const Usuario = require('../models/usuario-model')
-
+/* const Usuario = require('../models/usuario-model') */
+const usuariosDao = require('../DAO/usuarios-dao')
 
 module.exports = (app, bd) => {
     app.get('/usuarios', (req, resp) => 
         {
+            const classe = new usuariosDao(bd)
+            classe.listaUsuarios()
+                .then((usuarios) => {
+                    resp.send(usuarios)
+                })
 
-            bd.all("SELECT * FROM USUARIOS;", (error, rows)=>{
-                if (error) throw new Error("Erro ao consultar tabela.") 
-                else resp.send(rows)
-            }) 
+                .catch((error) =>{
+                    resp.send(error)
+                })
 
+ 
         }
     )
 
@@ -27,9 +32,18 @@ module.exports = (app, bd) => {
 
     app.post('/usuarios', (req, resp) => 
         {
-            const usr = new Usuario(req.body.nome, req.body.email, req.body.senha)
+            /* const usr = new Usuario(req.body.nome, req.body.email, req.body.senha)
             bd.usuariosBD.push(usr)
-            resp.send('<h1>Usuário adicionado ao banco de dados</h1>')
+            resp.send('<h1>Usuário adicionado ao banco de dados</h1>') */
+
+            let array =  [req.body.NOME, req.body.EMAIL, req.body.SENHA]
+
+            bd.run("INSERT INTO USUARIOS (NOME, EMAIL, SENHA) VALUES (?,?,?)", array, (error) => {
+                if (error) throw new Error(`Erro ${error} ao inserir valores.`) 
+                else resp.send("Usuário inserido com sucesso")
+
+            })
+
         }
     )
 
